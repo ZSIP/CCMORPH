25df318a8b1c1786073cd82e3635243cc450cec7
wmackow-zut <wmackow@DESKTOP-HARVESTER.localdomain>
2022-12-29 16:10:41 +0100

    readme.md update

--- example_2011.sh
+++ example_2011.sh
@@ -17,9 +17,9 @@ then
     echo -e "$YELLOW---> Example 2011: prepare input data$OFF"
     unzip -o example_2011_data.zip
     echo -e "$YELLOW---> Example 2011: prepare configs$OFF"
-    cp example_2011_data/generator-py.config.json generator-py/config.json
-    cp example_2011_data/shaper-py.config.json shaper-py/config.json
-    cp example_2011_data/analyzer-py.config.json analyzer-py/config.json
+    mv example_2011_data/generator-py.config.json generator-py/config.json
+    mv example_2011_data/shaper-py.config.json shaper-py/config.json
+    mv example_2011_data/analyzer-py.config.json analyzer-py/config.json
 fi
 echo -e "$YELLOW---> Example 2011: run generator-py$OFF"; cd generator-py; python3 main.py &&
 ( echo -e "$YELLOW---> Example 2011: run shaper-py$OFF"; cd ../shaper-py; python3 main.py &&
--- example_2017.sh
+++ example_2017.sh
@@ -17,9 +17,9 @@ then
     echo -e "$YELLOW---> Example 2017: prepare input data$OFF"
     unzip -o example_2017_data.zip
     echo -e "$YELLOW---> Example 2017: prepare configs$OFF"
-    cp example_2017_data/generator-py.config.json generator-py/config.json
-    cp example_2017_data/shaper-py.config.json shaper-py/config.json
-    cp example_2017_data/analyzer-py.config.json analyzer-py/config.json
+    mv example_2017_data/generator-py.config.json generator-py/config.json
+    mv example_2017_data/shaper-py.config.json shaper-py/config.json
+    mv example_2017_data/analyzer-py.config.json analyzer-py/config.json
 fi
 echo -e "$YELLOW---> Example 2017: run generator-py$OFF"; cd generator-py; python3 main.py &&
 ( echo -e "$YELLOW---> Example 2017: run shaper-py$OFF"; cd ../shaper-py; python3 main.py &&
