e0203ea69252a89761b99dcf307212af8ed2c8f3
wmackow-zut <wmackow@zut.edu.pl>
2024-02-07 12:10:12 +0100

    minor init.sh change

--- init.sh
+++ init.sh
@@ -12,3 +12,15 @@ echo -e "$YELLOW---> Install requirements (Python libraries)$OFF"
 GV=`gdalinfo --version | grep -oE "[0-9]+\.[0-9]+\.[0-9]+"`
 echo "GDAL==$GV" | cat requirements - > requirements.txt
 pip install -r requirements.txt
+
+echo -e "$YELLOW---> Prepare demo data$OFF"
+cat demo/sample_1/input/dem/sample_1.tif_* > demo/sample_1/input/dem/sample_1.tif
+cat demo/sample_2/input/dem/sample_2.tif_* > demo/sample_2/input/dem/sample_2.tif
+cat demo/sample_3/input/dem/sample_3.tif_* > demo/sample_3/input/dem/sample_3.tif
+cat demo/sample_4/input/dem/sample_4.tif_* > demo/sample_4/input/dem/sample_4.tif
+cat demo/sample_5/input/dem/sample_5.tif_* > demo/sample_5/input/dem/sample_5.tif
+rm demo/sample_1/input/dem/sample_1.tif_*
+rm demo/sample_2/input/dem/sample_1.tif_*
+rm demo/sample_3/input/dem/sample_1.tif_*
+rm demo/sample_4/input/dem/sample_1.tif_*
+rm demo/sample_5/input/dem/sample_1.tif_*
